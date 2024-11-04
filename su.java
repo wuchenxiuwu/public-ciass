77d239f313fb5a9fe961e48a04b1c2e737159708
wuchenxiuwu <116479750+wuchenxiuwu@users.noreply.github.com>
2024-11-04 21:44:37 +0800

    java.nnnn

---
+++ su.java
@@ -0,0 +1,12 @@
+pblic class Main {
+   public static void main (String [] args ){
+      int a = 2.7;
+      int b = 7.3;
+      int c = a / b + a;
+      System.out.println(c);
+//注释一部分
+      System.out.println("直接注释掉");
+
+  }
+
+}
